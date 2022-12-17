a9c05e98cf8500ad8a0c0dc53f04f677103201f6
Wilbert Santos <wsm007@gmail.com>
2022-12-16 23:54:20 -0500

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    prueba = models.TextField(blank=True, null=True)
+
+    # def __str__(self):
+    #     return self.title + ' - by ' + self.user.username
